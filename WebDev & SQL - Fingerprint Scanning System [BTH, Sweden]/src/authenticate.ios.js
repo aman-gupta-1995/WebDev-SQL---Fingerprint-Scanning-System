c75f0fe51007e85e5a9523153df9f54a997f5eff
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-19 09:45:44 +0530

    Update authenticate.ios.js

--- WebDev & SQL - Fingerprint Scanning System [BTH, Sweden]/src/authenticate.ios.js
+++ WebDev & SQL - Fingerprint Scanning System [BTH, Sweden]/src/authenticate.ios.js
@@ -2,7 +2,7 @@ import { NativeModules } from 'react-native';
 import createError from './createError';
 
 const { ReactNativeFingerprintScanner } = NativeModules;
-
+ 
 export default ({ description = ' ', fallbackEnabled = true }) => { 
   return new Promise((resolve, reject) => { 
     ReactNativeFingerprintScanner.authenticate(description, fallbackEnabled, error => {
